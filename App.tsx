aeaae4d737c137b038da385e6b31b36f1bc23ae7
Thalyson Luiz <thalysonluiz@gmail.com>
2023-01-18 07:43:36 -0300

    fonts, Loading Component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold
+} from '@expo-google-fonts/inter'
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <Loading />
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center',
   },
